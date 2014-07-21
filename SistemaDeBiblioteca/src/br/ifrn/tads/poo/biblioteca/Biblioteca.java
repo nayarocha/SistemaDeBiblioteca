d48e941a8c508bffc8fb197283e7f8a6cfb39477
nayarocha <nayaarocha@gmail.com>
2014-07-21 16:15:06 -0300

    Mudança no arquivo Biblioteca.java

--- SistemaDeBiblioteca/src/br/ifrn/tads/poo/biblioteca/Biblioteca.java
+++ SistemaDeBiblioteca/src/br/ifrn/tads/poo/biblioteca/Biblioteca.java
@@ -6,12 +6,48 @@ import br.ifrn.tads.poo.biblioteca.usuario.Usuario;
 
 public class Biblioteca {
 	private String nomeBiblioteca;	
+	
+	//private Usuario usuario;
+	// ItemAcervo itemAcervo;
+	
 	private ItemAcervo acervoBiblioteca[] = new ItemAcervo[50];
 	private Usuario usuarios[] = new Usuario[10];
+	private int quantUsuarios;
+	private int quantItens;
 	
 	public Biblioteca(){
 
 	}
+	
+	//CADASTRA USUARIOS 
+	public void cadastraUsuario(Usuario usuario){
+		for (int i = 0; i < usuarios.length; i++) {
+			if(usuarios[i]==null){
+				usuarios[i] = usuario;
+				
+			}
+			
+		}
+		quantUsuarios++;
+		System.out.println("Usuario cadastrado");
+		System.out.println("Quantidade de usuarios cadastrados:" +quantUsuarios);
+	}
+	
+	
+	//CADASTRAR LIVROS
+	public void cadastraItens(ItemAcervo itemacervo){
+		for (int i = 0; i < acervoBiblioteca.length; i++) {
+			if(acervoBiblioteca[i]==null){
+				acervoBiblioteca[i] = itemacervo;
+				
+			}
+			
+		}
+		quantItens++;
+		System.out.println("Item  cadastrado");
+		System.out.println("Quantidade de itens cadastrados:" +quantItens);
+	}
+	
 	
 	//GERANDO ACERVO	
 	public void gerarAcervo(){
@@ -19,6 +55,7 @@ public class Biblioteca {
 			
 		}
 	}
+	
 	
 	
 	//CALCULAR VALOR DAS MULTAS
